3daa6de825ce531225ed4a8a23c15d3d72a0a810
RDHB <renedav_12@hotmail.es>
2018-12-07 17:13:55 -0600

    Funcion de Crgar Instrucciones y Memoria

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ document.getElementById('fInstrucciones').addEventListener('change', leerArchivo
 
 function leerArchivo(e) {
   $('#tInstrucciones').empty();
+  $('#cmInstrucciones').empty();
   var archivo = e.target.files[0];
   if (!archivo) {
   }
@@ -23,7 +24,7 @@ function mostrarContenido(contenido) {
   var lineas = contenido.split('\n');
   var contador=0;
   var n=lineas.length-1;
-  var error=0;
+  var error=[];
   for (var linea of lineas){
 	  elemento. insertAdjacentHTML('beforeend','<tr><th scope="row">'+contador+'</th><td id="tdE'+contador+'">'+linea+'</td></tr>');
 	  if(n==contador){
@@ -32,24 +33,22 @@ function mostrarContenido(contenido) {
 			  var lineaMala=document.getElementById('tdE'+contador);
 			  lineaMala.style.background="rgba(122,0,0,0.2)";
 			  lineaMala.previousSibling.style.background="rgba(122,0,0,0.2)";
-			  error=0;
+			  error.push(1);
 		  }else{
 			  if(linea.length>5){
 				  alert("Tiene un error en la instruccion numero "+contador+", por favor revise la instruccion y vuelva a intentar ");
 				  var lineaMala=document.getElementById('tdE'+contador);
 				  lineaMala.style.background="rgba(122,0,0,0.2)";
 				  lineaMala.previousSibling.style.background="rgba(122,0,0,0.2)";
-				  error=0;
+				  error.push(1);
 			  }else{
 				  if(linea.substr(0,2)==10 || linea.substr(0,2)==11 || linea.substr(0,2)==20 || linea.substr(0,2)==21 || linea.substr(0,2)==30 || linea.substr(0,2)==31 || linea.substr(0,2)==32 || linea.substr(0,2)==33 || linea.substr(0,2)==40 || linea.substr(0,2)==41 || linea.substr(0,2)==42 || linea.substr(0,2)==43){
-					  error=1;
-					  
 				  }else{
 					  alert("Tiene un error en la instruccion numero "+contador+", por favor revise el codigo de la instruccion y vuelva a intentar ");
 					  var lineaMala=document.getElementById('tdE'+contador);
 					  lineaMala.style.background="rgba(122,0,0,0.2)";
 					  lineaMala.previousSibling.style.background="rgba(122,0,0,0.2)";
-					  error=0;
+					  error.push(1);
 				  }
 			  }  
 		  }
@@ -60,23 +59,22 @@ function mostrarContenido(contenido) {
 			  var lineaMala=document.getElementById('tdE'+contador);
 			  lineaMala.style.background="rgba(122,0,0,0.2)";
 			  lineaMala.previousSibling.style.background="rgba(122,0,0,0.2)";
-			  error=0;
+			  error.push(1);
 		  }else{
 			  if(linea.length>6){
 				  alert("Tiene un error en la instruccion numero "+contador+", por favor revise la instruccion y vuelva a intentar ");
 				  var lineaMala=document.getElementById('tdE'+contador);
 				  lineaMala.style.background="rgba(122,0,0,0.2)";
 				  lineaMala.previousSibling.style.background="rgba(122,0,0,0.2)";
-				  error=0;
+				  error.push(1);
 			  }else{
 				  if(linea.substr(0,2)==10 || linea.substr(0,2)==11 || linea.substr(0,2)==20 || linea.substr(0,2)==21 || linea.substr(0,2)==30 || linea.substr(0,2)==31 || linea.substr(0,2)==32 || linea.substr(0,2)==33 || linea.substr(0,2)==40 || linea.substr(0,2)==41 || linea.substr(0,2)==42 || linea.substr(0,2)==43){
-					  error=1;
 				  }else{
 					  alert("Tiene un error en la instruccion numero "+contador+", por favor revise el codigo de la instruccion y vuelva a intentar ");
 					  var lineaMala=document.getElementById('tdE'+contador);
 					  lineaMala.style.background="rgba(122,0,0,0.2)";
 					  lineaMala.previousSibling.style.background="rgba(122,0,0,0.2)";
-					  error=0;
+					  error.push(1);
 				  }
 			  }
 				  
@@ -84,14 +82,34 @@ function mostrarContenido(contenido) {
 	  }
       contador=contador+1;
   }  
-  puedeCargar=error;
+  if(error.length==0){
+	puedeCargar=1; 
+  }else{
+	  puedeCargar=0;
+  }
+  
 }
 
-//########################################CODIGO PARA LEER DATOS DE COLUMNAS Y TRASLADARLO A MEMORIA###################################################################
+//########################################CODIGO PARA LEER DATOS DE ENTRADA Y TRASLADARLO A MEMORIA###################################################################
 $(document).ready(function(){
 	$('#bCargarInst').click(function(){
 	    if(puedeCargar==1){
-			alert("Exito");
+            var n = document.querySelector('#tInstrucciones').querySelectorAll('tr').length;
+			var elemento = document.getElementById('cmInstrucciones');
+			for(i=0;i<n;i++){
+				var entrada=$('#tdE'+i)[0].innerHTML;
+				if(i<10){
+					elemento.insertAdjacentHTML('beforeend','<tr><th scope="row">00'+i+'</th><td id="tdM00'+i+'">'+entrada+'</td></tr>');
+				}else{
+					if(i<100){
+						elemento.insertAdjacentHTML('beforeend','<tr><th scope="row">0'+i+'</th><td id="tdM0'+i+'">'+entrada+'</td></tr>');
+					}else{
+						if(i<1000){
+							elemento.insertAdjacentHTML('beforeend','<tr><th scope="row">'+i+'</th><td id="tdM'+i+'">'+entrada+'</td></tr>');
+						}
+					}
+				}
+			}	
 		}else{
 			alert("Revise el conjunto de instrucciones, puede que no tenga ninguna instruccion cargada o tenga error el conjunto de instrucciones cargadas (las casillas marcadas en rojo indica un posible error)");
 		}
